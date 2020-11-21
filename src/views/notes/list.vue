224a4ce21276c90867e191128e10c8a2062069b0
scma <59968873@qq.com>
2020-11-22 00:34:20 +0800

    [提交代码] 笔记列表页面整理

---
+++ src/views/notes/list.vue
@@ -0,0 +1,198 @@
+<template>
+    <div id="aCoursesList" class="bg-fa of">
+        <Header></Header>
+
+        <!-- 讲师列表 开始 -->
+        <section class="container">
+            <header class="comm-title all-teacher-title">
+
+            <h2 class="fl tac">
+                    <span class="c-333">全部笔记</span>
+                </h2>
+                <!--<section class="c-tab-title">
+                  <a id="subjectAll" title="全部" href="#">全部</a>
+                  &lt;!&ndash; <c:forEach var="subject" items="${subjectList }">
+                                    <a id="${subject.subjectId}" title="${subject.subjectName }" href="javascript:void(0)" onclick="submitForm(${subject.subjectId})">${subject.subjectName }</a>
+                  </c:forEach>&ndash;&gt;
+                </section>-->
+                <section class="c-sort-box">
+                    <section class="c-s-dl">
+                        <dl>
+                            <dt>
+                                <span class="c-999 fsize14">课程类别</span>
+                            </dt>
+                            <dd class="c-s-dl-li">
+                                <ul class="clearfix">
+                                    <li>
+                                        <a title="全部" href="#">全部</a>
+                                    </li>
+                                    <li v-for="(item, index) in subjectNestedList" :key="index" :class="{active:oneIndex==index}">
+                                        <a :title="item.title" href="#" @click="searchOne(item.id,index)">{{ item.title }}</a>
+                                    </li>
+
+                                </ul>
+                            </dd>
+                        </dl>
+                        <dl>
+                            <dt>
+                                <span class="c-999 fsize14"/>
+                            </dt>
+                            <dd class="c-s-dl-li">
+                                <ul class="clearfix">
+                                    <li v-for="(item, index) in subSubjectList" :key="index" :class="{active:twoIndex==index}">
+                                        <a :title="item.title" href="#" @click="searchTwo(item.id,index)">{{ item.title }}</a>
+                                    </li>
+
+                                </ul>
+                            </dd>
+                        </dl>
+                        <div class="clear"/>
+                    </section>
+                    <div class="js-wrap">
+                        <section class="fr">
+              <span class="c-ccc">
+                <i class="c-master f-fM">1</i>/
+                <i class="c-666 f-fM">1</i>
+              </span>
+                        </section>
+                        <section class="fl">
+                            <ol class="js-tap clearfix">
+                                <li :class="{'current bg-orange':buyCountSort!=''}">
+                                    <a title="销量" href="javascript:void(0);" @click="searchBuyCount()">销量
+                                        <span :class="{hide:buyCountSort==''}">↓</span>
+                                    </a>
+                                </li>
+                                <li :class="{'current bg-orange':gmtCreateSort!=''}">
+                                    <a title="最新" href="javascript:void(0);" @click="searchGmtCreate()">最新
+                                        <span :class="{hide:gmtCreateSort==''}">↓</span>
+                                    </a>
+                                </li>
+                                <li :class="{'current bg-orange':priceSort!=''}">
+                                    <a title="价格" href="javascript:void(0);" @click="searchPrice()">价格&nbsp;
+                                        <span :class="{hide:priceSort==''}">↓</span>
+                                    </a>
+                                </li>
+                            </ol>
+                        </section>
+                    </div>
+                </section>
+            </header>
+
+            <!--                <section class="c-sort-box unBr">-->
+<!--                <div>-->
+<!--                    &lt;!&ndash; /无数据提示 开始&ndash;&gt;-->
+<!--                    <section v-if="data.total==0" class="no-data-wrap">-->
+<!--                        <em class="icon30 no-data-ico">&nbsp;</em>-->
+<!--                        <span class="c-666 fsize14 ml10 vam">没有相关数据，小编正在努力整理中...</span>-->
+<!--                    </section>-->
+<!--                    &lt;!&ndash; /无数据提示 结束&ndash;&gt;-->
+<!--                    <article v-if="data.total>0" class="i-teacher-list">-->
+<!--                        <ul class="of">-->
+<!--                            <li v-for="teacher in data.items" :key="teacher.id">-->
+<!--                                <section class="i-teach-wrap">-->
+<!--                                    <div class="i-teach-pic">-->
+<!--                                        <a :title="teacher.name" :href="'/teacher/'+teacher.id" target="_blank">-->
+<!--                                            <img :src="teacher.avatar" :alt="teacher.name">-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                    <div class="mt10 hLh30 txtOf tac">-->
+<!--                                        <a :title="teacher.name" :href="'/teacher/'+teacher.id" target="_blank" class="fsize18 c-666">{{ teacher.name }}</a>-->
+<!--                                    </div>-->
+<!--                                    <div class="hLh30 txtOf tac">-->
+<!--                                        <span class="fsize14 c-999">{{ teacher.intro }}</span>-->
+<!--                                    </div>-->
+<!--                                    <div class="mt15 i-q-txt">-->
+<!--                                        <p class="c-999 f-fA">{{ teacher.career }}</p>-->
+<!--                                    </div>-->
+<!--                                </section>-->
+<!--                            </li>-->
+
+<!--                        </ul>-->
+<!--                        <div class="clear"/>-->
+<!--                    </article>-->
+<!--                </div>-->
+                <!-- 公共分页 开始 -->
+                <!-- 公共分页 开始 -->
+<!--                <div>-->
+<!--                    <div class="paging">-->
+<!--                        &lt;!&ndash; undisable这个class是否存在，取决于数据属性hasPrevious &ndash;&gt;-->
+<!--                        <a-->
+<!--                                :class="{undisable: !data.hasPrevious}"-->
+<!--                                href="#"-->
+<!--                                title="首页"-->
+<!--                                @click.prevent="gotoPage(1)">首</a>-->
+<!--                        <a-->
+<!--                                :class="{undisable: !data.hasPrevious}"-->
+<!--                                href="#"-->
+<!--                                title="前一页"-->
+<!--                                @click.prevent="gotoPage(data.current-1)">&lt;</a>-->
+<!--                        <a-->
+<!--                                v-for="page in data.pages"-->
+<!--                                :key="page"-->
+<!--                                :class="{current: data.current == page, undisable: data.current == page}"-->
+<!--                                :title="'第'+page+'页'"-->
+<!--                                href="#"-->
+<!--                                @click.prevent="gotoPage(page)">{{ page }}</a>-->
+<!--                        <a-->
+<!--                                :class="{undisable: !data.hasNext}"-->
+<!--                                href="#"-->
+<!--                                title="后一页"-->
+<!--                                @click.prevent="gotoPage(data.current+1)">&gt;</a>-->
+<!--                        <a-->
+<!--                                :class="{undisable: !data.hasNext}"-->
+<!--                                href="#"-->
+<!--                                title="末页"-->
+<!--                                @click.prevent="gotoPage(data.pages)">末</a>-->
+<!--                        <div class="clear"/>-->
+<!--                    </div>-->
+<!--                </div>-->
+                <!-- 公共分页 结束 -->
+<!--            </section>-->
+        </section>
+        <Footer></Footer>
+
+        <!-- /讲师列表 结束 -->
+    </div>
+</template>
+<script>
+    import Header from "@/components/Header";
+    import Footer from "@/components/Footer";
+
+    export default {
+        name: "Blogs.vue",
+        components: {Header,Footer},
+        data() {
+            return {
+                blogs: {},
+                currentPage: 1,
+                total: 0,
+                pageSize: 5
+            }
+        },
+        methods: {
+
+        },
+        created() {
+            // this.page(1)
+        }
+    }
+
+
+    // import teacherApi from '@/api/teacher'
+    // export default {
+    //     // 异步调用 且只调用一次
+    //     asyncData({ params, error }) {
+    //         return teacherApi.getTeacherFront(1, 8).then(response => {
+    //             return { data: response.data.data }
+    //         })
+    //     },
+    //     methods: {
+    //         // 分页切换效果
+    //         gotoPage(page) {
+    //             teacherApi.getTeacherFront(page, 8).then(response => {
+    //                 this.data = response.data.data
+    //             })
+    //         }
+    //     }
+    // }
+</script>
